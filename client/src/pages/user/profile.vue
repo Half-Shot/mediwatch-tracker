27e2373a97dc218d747e629ea1fd2fefcc7bcb97
Alex <alex324@hotmail.co.uk>
2018-11-15 14:56:35 +0000

    createdForm

--- client/src/pages/user/profile.vue
+++ client/src/pages/user/profile.vue
@@ -1,5 +1,25 @@
 <template lang="html">
+<div>
   <h1>Personal info</h1>
+    <form>
+      <label for="fName">First Name:</label>
+      <input type="text" id="fName">
+      </br>
+      <label for="lName">Last Name:</label>
+      <input type="text" id="lName">
+      </br>
+      <label for="dob">Date of Birth:</label>
+      <input type="date" id="dob">
+      </br>
+      <label for="weight">Weight(KG):</label>
+      <input type="text" id="weight">
+      </br>
+      <label for="height">Height(CM):</label>
+      <input type="text" id="height">
+      </br>
+      <input type="submit">
+    </form>
+</div>
 </template>
 
 <script>
