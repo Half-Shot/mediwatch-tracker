b17e25e6184bc57e90fe8e82e52c9bd362d3fda5
Ivan Lebanov <ivan.lebanov@thejustbrand.com>
2018-11-16 21:24:18 +0000

    login validator

--- client/src/main.js
+++ client/src/main.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Meta from 'vue-meta'
-import router from './router/index'
-import store from './store/index'
+import router from './router'
+import store from './store'
 import MatrixClientPeg from './MatrixClientPeg'
+import VeeValidate from 'vee-validate';
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(Meta)
+Vue.use(VeeValidate)
 
 MatrixClientPeg.attemptToGetLoggedIn();
 
