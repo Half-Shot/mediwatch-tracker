14374d032fefcb64d466e43d755c8e98b9d6ea0d
Will Hunt <will@half-shot.uk>
2018-11-16 18:37:55 +0000

    Peg now lets you log in, woo hoo!

--- client/src/MatrixClientPeg.js
+++ client/src/MatrixClientPeg.js
@@ -1,11 +1,32 @@
 /**
  * This class will hold the matrix client
  */
-import * as Client from "matrix-js-sdk";
+import * as Matrix from "matrix-js-sdk";
+
+export const E_NOLOGIN = 1;
 
 export class MatrixClientPeg {
     constructor() {
         this.client = null;
+    }
+
+    async attemptToGetLoggedIn() {
+        const accessToken = window.localStorage["mx_accesstoken"];
+        const baseUrl = window.localStorage["mx_url"];
+
+        if (!accessToken || !baseUrl) {
+            throw E_NOLOGIN;
+        }
+
+        // We have enough to get going with, so get going.
+        this.config = Matrix.createClient({
+            accessToken,
+            baseUrl,
+        });
+
+        // Connect!
+        await client.startClient({initialSyncLimit: 10});
+        return client;
     }
 
     setClient(client) {
