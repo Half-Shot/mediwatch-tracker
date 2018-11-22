ff72e87ea28ccfb3b46f6c3cc4ab669f75475a1a
Alex <alex324@hotmail.co.uk>
2018-11-22 20:08:02 +0000

    styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -38,6 +38,7 @@ export default {
 
 <style lang="scss">
 @import "../node_modules/normalize.css/normalize.css";
+@import "../stylesheet.css";
 
 #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
