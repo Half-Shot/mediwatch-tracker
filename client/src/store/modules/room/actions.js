3ac5b640b5d8c022e319da82d382cce4529d17ee
Ivan Lebanov <ivan.lebanov@thejustbrand.com>
2018-11-27 15:58:36 +0000

    unsaved changes

--- client/src/store/modules/room/actions.js
+++ client/src/store/modules/room/actions.js
@@ -15,7 +15,6 @@ const TYPE_TO_NAME = {
 }
 
 export default {
-<<<<<<< HEAD
   fetchRooms({
     state,
     commit,
@@ -23,7 +22,6 @@ export default {
   }, data) {
     const acctRooms = {
       medicalLog: null,
-      medicalInfo: null
     };
     console.log("Fetching rooms");
     this.getters['auth/client'].getRooms().forEach((room) => {
@@ -35,12 +33,8 @@ export default {
         console.warn(`${room.roomId} has multiple roomtypes, eek`);
       }
       const rType = state[0].getContent().type;
-      console.log(room);
       if (rType === MEDICAL_LOG_TYPE) {
         acctRooms.medicalLog = room;
-      }
-      if (rType === MEDICAL_INFO_TYPE) {
-        acctRooms.medicalInfo = room;
       }
     });
     commit('SET_ROOMS', acctRooms);
@@ -50,16 +44,39 @@ export default {
     commit,
     dispatch
   }, data) {
-    let title = (data.title) ? `Mediwatch ${data.title}` : `Mediwatch ${TYPE_TO_NAME[data.type]}`;
+    const userId = this.getters['auth/client'].getUserId;
+    const users = {};
+    users[userId] = 100;
     return this.getters['auth/client'].createRoom({
-      name: title,
+      name: `Mediwatch ${TYPE_TO_NAME[data.type]}`,
       visibility: "private",
       initial_state: [{
-        type: STATE_TYPE_TYPE,
-        content: {
-          type: TYPE_TO_MX_TYPE[data.type]
+          type: STATE_TYPE_TYPE,
+          content: {
+            type: TYPE_TO_MX_TYPE[data.type]
+          },
+        },
+        {
+          type: "m.room.power_levels",
+          content: {
+            users,
+            "users_default": 0,
+            "events": {
+              "m.room.name": 50,
+              "m.room.power_levels": 100,
+              "m.room.history_visibility": 100,
+              "m.room.canonical_alias": 100,
+              "m.room.avatar": 100
+            },
+            "events_default": 50,
+            "state_default": 50,
+            "ban": 100,
+            "kick": 100,
+            "redact": 100,
+            "invite": 100
+          },
         }
-      }]
+      ]
     });
   },
   addToLog({
@@ -76,77 +93,3 @@ export default {
     );
   },
 };
-=======
-    fetchRooms({
-      state,
-      commit,
-      dispatch
-    }, data) {
-        const acctRooms = {
-            medicalLog: null,
-        };
-        console.log("Fetching rooms");
-        this.getters['auth/client'].getRooms().forEach((room) => {
-            const state = room.currentState.getStateEvents(STATE_TYPE_TYPE);
-            if (state.length === 0) { return; }
-            if (state.length > 1) {
-                console.warn(`${room.roomId} has multiple roomtypes, eek`);
-            }
-            const rType = state[0].getContent().type;
-            if (rType === MEDICAL_LOG_TYPE) {
-                acctRooms.medicalLog = room;
-            }
-        });
-        commit('SET_ROOMS', acctRooms);
-    },
-    create({
-      state,
-      commit,
-      dispatch
-    }, data) {
-        const userId = this.getters['auth/client'].getUserId;
-        const users = {};
-        users[userId] = 100;
-        return this.getters['auth/client'].createRoom({
-            name: `Mediwatch ${TYPE_TO_NAME[data.type]}`,
-            visibility: "private",
-            initial_state: [{
-                type: STATE_TYPE_TYPE,
-                content: {
-                    type: TYPE_TO_MX_TYPE[data.type]
-                },
-            },
-            {
-                type: "m.room.power_levels",
-                content: {
-                    users,
-                    "users_default": 0,
-                    "events": {
-                        "m.room.name": 50,
-                        "m.room.power_levels": 100,
-                        "m.room.history_visibility": 100,
-                        "m.room.canonical_alias": 100,
-                        "m.room.avatar": 100
-                    },
-                    "events_default": 50,
-                    "state_default": 50,
-                    "ban": 100,
-                    "kick": 100,
-                    "redact": 100,
-                    "invite": 100
-                },
-            }]
-        });
-    },
-    addToLog({
-      state,
-      commit,
-      dispatch
-  }, {room, body}) {
-        return this.getters['auth/client'].sendTextMessage(
-            room.roomId,
-            body,
-        );
-    },
-};
->>>>>>> d9374a36fe82a4a2949c67d8e050ddb3958bf8f2
