c6d9541ca98b6b38e8332cb40a9872ad7380fabd
Will Hunt <will@half-shot.uk>
2018-11-27 15:51:12 +0000

    Some stuff for showing rooms

--- client/src/pages/Room.vue
+++ client/src/pages/Room.vue
@@ -1,23 +1,36 @@
 <template>
 <div class="container">
-    Hello this is a room.
+    <BaseRoom v-if="room" :room="room">
+    </BaseRoom>
 </div>
 </template>
 
 <script>
+import BaseRoom from "@/components/Room"
 export default {
-  props: ['room', 'roomType'],
+  props: ['roomType'],
   metaInfo: {
     title: 'Room'
   },
+  components: {
+    BaseRoom,
+  },
   data: function() {
     return {
-
+      room: null
     }
   },
   mounted() {
-    console.log("Got room of type", this.roomType, ":", this.room);
-
+    this.$store.dispatch('room/fetchRooms');
+    const roomType = this.$route.params.roomType;
+    const r = this.$store.getters['room/roomSet'][roomType];
+    console.log("Got room of type", roomType, ":", r);
+    if (!r) {
+      this.$router.push({
+        name: "home"
+      });
+    }
+    this.room = r;
   },
   methods: {
 
