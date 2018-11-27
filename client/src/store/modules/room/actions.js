d9374a36fe82a4a2949c67d8e050ddb3958bf8f2
Will Hunt <will@half-shot.uk>
2018-11-27 15:51:25 +0000

    Set proper permissions when creating rooms.

--- client/src/store/modules/room/actions.js
+++ client/src/store/modules/room/actions.js
@@ -40,6 +40,9 @@ export default {
       commit,
       dispatch
     }, data) {
+        const userId = this.getters['auth/client'].getUserId;
+        const users = {};
+        users[userId] = 100;
         return this.getters['auth/client'].createRoom({
             name: `Mediwatch ${TYPE_TO_NAME[data.type]}`,
             visibility: "private",
@@ -47,7 +50,27 @@ export default {
                 type: STATE_TYPE_TYPE,
                 content: {
                     type: TYPE_TO_MX_TYPE[data.type]
-                }
+                },
+            },
+            {
+                type: "m.room.power_levels",
+                content: {
+                    users,
+                    "users_default": 0,
+                    "events": {
+                        "m.room.name": 50,
+                        "m.room.power_levels": 100,
+                        "m.room.history_visibility": 100,
+                        "m.room.canonical_alias": 100,
+                        "m.room.avatar": 100
+                    },
+                    "events_default": 50,
+                    "state_default": 50,
+                    "ban": 100,
+                    "kick": 100,
+                    "redact": 100,
+                    "invite": 100
+                },
             }]
         });
     },
